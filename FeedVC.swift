e34caf2366a6339a42f7103bd71fd149aa048b40
maya-droid <69542364+maya-droid@users.noreply.github.com>
2020-08-16 04:06:32 +0400

    Add files via upload

---
+++ FeedVC.swift
@@ -0,0 +1,121 @@
+//
+//  FeedVC.swift
+//  BeirutTrackingApp
+//
+//  Created by SPEAKFLUENCE GLOBAL on 13/08/2020.
+//  Copyright © 2020 Maya Bridgman. All rights reserved.
+//
+
+
+import UIKit
+import Firebase
+import FirebaseDatabase
+import SwiftKeychainWrapper
+import FirebaseAuth
+
+class FeedVC: UITableViewController, UISearchBarDelegate{
+    
+    @IBOutlet weak var searchBar: UISearchBar!
+    
+    var currentUserImageUrl: String!
+    var posts = [postStruct]()
+    var selectedPost: Post!
+    var filteredPosts: [String]!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        getUsersData()
+        getPosts()
+        searchBar.delegate = self
+       // filteredPosts = posts
+        // Do any additional setup after loading the view.
+   //     tableView.register(PostCell.self, forCellReuseIdentifier: "PostCell")
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+    func getUsersData(){
+
+        guard let userID = Auth.auth().currentUser?.uid else { return }
+        Database.database().reference().child("users").child(userID).observeSingleEvent(of: .value) { (snapshot) in
+            if let postDict = snapshot.value as? [String : AnyObject] {
+                self.tableView.reloadData()
+            }
+        }
+    }
+    
+    struct postStruct {
+        let name : String!
+        let contactEmail : String!
+        let contactPhoneNum : String!
+        let age : String!
+        let gender : String!
+        let lastSeen : String!
+        let profileDescription : String!
+    }
+    
+    func getPosts() {
+        let databaseRef = Database.database().reference()
+        databaseRef.child("firstName").queryOrderedByKey().observe( .childAdded, with: {
+                    snapshot in
+                    let name = (snapshot.value as? NSDictionary)!["name"] as? String
+                    let contactEmail = (snapshot.value as? NSDictionary
+                    )!["contactEmail"] as? String
+                    let contactPhoneNum = (snapshot.value as? NSDictionary
+                    )!["contactPhoneNum"] as? String
+                    let age = (snapshot.value as? NSDictionary
+                    )!["age"] as? String
+                    let gender = (snapshot.value as? NSDictionary
+                    )!["gender"] as? String
+                    let lastSeen = (snapshot.value as? NSDictionary
+                    )!["lastSeen"] as? String
+                    let profileDescription = (snapshot.value as? NSDictionary
+                    )!["profileDescription"] as? String
+            self.posts.append(postStruct(name: name,contactEmail:contactEmail, contactPhoneNum:contactPhoneNum, age:age, gender:gender, lastSeen:lastSeen, profileDescription:profileDescription))
+            DispatchQueue.main.async {
+                        self.tableView.reloadData()
+                    }
+                })
+    }
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        print("posts count = ", posts.count)
+        return posts.count
+
+   }
+    
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+      //  tableView.dequeueReusableCell(withIdentifier: "PostCell")!.frame.size.height
+        return 230
+    }
+   override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "PostCell") as? PostCell else { return UITableViewCell() }
+        cell.nameLabel?.text = posts[indexPath.row].name
+        cell.contactEmailLabel?.text = posts[indexPath.row].contactEmail
+        cell.contactPhoneNumLabel?.text = posts[indexPath.row].contactPhoneNum
+        cell.ageLabel?.text = posts[indexPath.row].age
+        cell.genderLabel?.text = posts[indexPath.row].gender
+        cell.lastSeenLabel?.text = posts[indexPath.row].lastSeen
+        cell.profileDescriptionLabel?.text = posts[indexPath.row].profileDescription
+        print(posts)
+        return cell
+    }
+    
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        filteredPosts = []
+      //  for name in posts{
+        //    if name.lowercased().contains(searchText.lowercased()){
+          //      filteredPosts.append(name)
+            }
+      //  }
+    //}
+}
+
---
+++ createPostViewController.swift
@@ -0,0 +1,129 @@
+import UIKit
+import Firebase
+import FirebaseAuth
+import FirebaseDatabase
+import FirebaseStorage
+
+class createPostViewController: UIViewController {
+    
+    
+    @IBOutlet weak var profileImage: UIImageView!
+    
+    @IBOutlet weak var contactEmail: UITextField!
+    @IBOutlet weak var contactPhoneNum: UITextField!
+    @IBOutlet weak var name: UITextField!
+    @IBOutlet weak var age: UITextField!
+    @IBOutlet weak var gender: UITextField!
+    @IBOutlet weak var lastSeen: UITextField!
+    @IBOutlet weak var profileDescription: UITextField!
+    @IBOutlet weak var postBtn: UIButton!
+    
+    var imagePicker: UIImagePickerController!
+    var selectedImage: UIImage!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(createPostViewController.handleSelect))
+        profileImage.addGestureRecognizer(tapGesture)
+        imagePicker = UIImagePickerController()
+        imagePicker.allowsEditing = true
+        imagePicker.delegate = self
+        postBtn.layer.cornerRadius = 10
+        // Do any additional setup after loading the view.
+    }
+    
+    
+
+    @objc func handleSelect(){
+        let pickerController = UIImagePickerController()
+        pickerController.delegate = self
+        present(pickerController, animated: true, completion: nil)
+    }
+    
+    
+    
+    
+    @IBAction func post(_ sender: AnyObject) {
+        let userID = Auth.auth().currentUser?.uid
+        
+        Database.database().reference().child("profiles").child(userID!).observe(.value, with: { (snapshot) in
+            let post: Dictionary<String, AnyObject> = [
+               // "userImg":  as AnyObject,
+                "name": self.name.text as AnyObject,
+                "contactEmail": self.contactEmail.text as AnyObject,
+                "contactPhoneNum": self.contactPhoneNum.text as AnyObject,
+                "age": self.age.text as AnyObject,
+                "gender": self.gender.text as AnyObject,
+                "lastSeen": self.lastSeen.text as AnyObject,
+                "profileDescription": self.profileDescription.text as AnyObject,
+            ]
+            
+            let firebasePost = Database.database().reference().child("profiles").childByAutoId()
+            firebasePost.setValue(post)
+        })
+        
+    }
+    
+    
+    
+    func uploadImageToFireBase(image: UIImage) {
+        // Create the file metadata
+        let metadata = StorageMetadata()
+        metadata.contentType = "image/jpeg"
+        
+        // Upload the file to the path FILE_NAME
+        Storage.storage().reference().child("FILE_NAME").putData(image.jpegData(compressionQuality: 0.42)!, metadata: metadata) { (metadata, error) in
+            guard let metadata = metadata else {
+              // Uh-oh, an error occurred!
+              print((error?.localizedDescription)!)
+              return
+            }
+            // Metadata contains file metadata such as size, content-type.
+            let size = metadata.size
+            
+            print("Upload size is \(size)")
+            print("Upload success")
+        }
+    }
+    
+
+    
+    
+    //func uploadToCloud(fileURL : URL) {
+      //  let storage = Storage.storage()
+        
+        //let data = Data()
+        
+        //let storageRef = storage.reference()
+        
+        //let localFule = fileURL
+        
+        //let userID = Auth.auth().currentUser?.uid
+        
+        //let photoRef = storageRef.child("profilePhoto\(userID)")
+        
+        //let uploadTask = photoRef.putFile(from: localFule, metadata: nil) { (metadata, err) in
+          //  guard let metadata = metadata else {
+            //    print(err?.localizedDescription)
+              //  return
+            //}
+            //print("Photo Upload")
+            
+       // }
+   // }
+}
+
+extension createPostViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate{
+      func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        if let image = info[UIImagePickerController.InfoKey.editedImage.rawValue] as? UIImage {
+              selectedImage = image
+              profileImage.image = image
+              uploadImageToFireBase(image: image)
+          } else {
+              print("image wasnt selected")
+          }
+
+        dismiss(animated: true, completion: nil)
+    }
+}
